e181a85542369b6b0caa948d2b8a44f6e12fe1c8
JeniaGurman <144277083+JeniaGurman@users.noreply.github.com>
2024-06-15 13:45:36 +0300

    homework 4

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,14 +2,13 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(3).max(20).email().required(),
+  email: Joi.string().min(3).max(20).email(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string()
     .min(3)
     .max(20)
-    .valid('work', 'home', 'personal')
-    .required(),
+    .valid('work', 'home', 'personal'),
 });
 
 export const updateContactSchema = Joi.object({
